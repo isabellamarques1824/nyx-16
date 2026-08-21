13071272f1027ec3907c86b22f5d1e19de8ab22d
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-21 11:11:52 -0300

    começando a ISA, enum RegisterCode

---
+++ include/isa.h
@@ -0,0 +1,16 @@
+#ifndef ISA_H
+#define ISA_H
+
+typedef enum{
+    REG_R0 = 0,
+    REG_R1 = 1,
+    REG_R2 = 2, 
+    REG_R3 = 3,
+    REG_R4 = 4,
+    REG_R5 = 5,
+    REG_R6 = 6, 
+    REG_R7 = 7
+
+}RegisterCode;
+
+#endif
